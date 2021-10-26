6031eb213ae203fbdd326a6b1af15b9618e3f851
Maen22 <m3n.ibregeth@gmail.com>
2021-10-27 00:40:28 +0300

    more styling.

--- src/screens/GameOverScreen.tsx
+++ src/screens/GameOverScreen.tsx
@@ -1,17 +1,29 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+import colors from "../constants/colors";
 import defaultStyles from "../constants/default-styles";
 
 const GameOverScreen: React.FC<Props> = (props) => {
   return (
     <View style={styles.screen}>
-      <Text style={defaultStyles.bodyText}>The Game is Over!</Text>
-      <Text style={defaultStyles.bodyText}>
-        Number of rounds: {props.roundsNumber}
-      </Text>
-      <Text style={defaultStyles.bodyText}>
-        The Number was: {props.userNumber}
-      </Text>
+      <Text style={defaultStyles.title}>The Game is Over!</Text>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/success.png")}
+          // IF IMAGE FROM WEB => source={{uri: ''}}
+          resizeMode="cover"
+        />
+      </View>
+      <View style={styles.resultContainer}>
+        <Text style={{ ...defaultStyles.bodyText, ...styles.resultText }}>
+          Your phone needed:{" "}
+          <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to
+          guess the number{" "}
+          <Text style={styles.highlight}>{props.userNumber}</Text>
+        </Text>
+      </View>
+
       <Button title="NEW GAME" onPress={props.onRestart} />
     </View>
   );
@@ -22,6 +34,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    overflow: "hidden",
+    marginVertical: 30,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  resultContainer: {
+    marginHorizontal: 30,
+    marginVertical: 15,
+  },
+  resultText: {
+    textAlign: "center",
+    fontSize: 20,
+  },
+  highlight: {
+    color: colors.primary,
+    fontFamily: "open-sans-bold",
   },
 });
 
